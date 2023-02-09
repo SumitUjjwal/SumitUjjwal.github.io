b85083ae349ea9b1fd0f660a7fe48979ad8959e1
SumitUjjwal <sumitonboard@gmail.com>
2023-02-09 15:42:19 +0530

    modified html file

---
+++ script/index.js
@@ -0,0 +1,9 @@
+// const Typed = require("typed")
+
+// // auto type section
+// let typed = new Typed('.auto-typed',{
+//     strings: ['Sumit,'],
+//     typeSpeed: 220,
+//     backSpeed: 220,
+//     loop: true,
+// });
