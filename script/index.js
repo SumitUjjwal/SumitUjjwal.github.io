747a5e64a994dc212c3122fc47b1aa101aa965b0
SumitUjjwal <sumitonboard@gmail.com>
2023-02-12 14:24:31 +0530

    worked on project section

--- script/index.js
+++ script/index.js
@@ -2,12 +2,12 @@ let ham_menu = document.querySelector("#ham-menu>span");
 let resume_btn = document.querySelector(".resume");
 
 function openNav() {
-    if(ham_menu.innerText == "menu"){
+    if (ham_menu.innerText == "menu") {
         ham_menu.innerText = "close";
         document.getElementById("mySidenav").style.width = "100%";
-        
+
     }
-    else{
+    else {
         document.getElementById("mySidenav").style.width = "0";
         ham_menu.innerText = "menu";
     }
@@ -26,4 +26,38 @@ GitHubCalendar(".calendar", "sumitujjwal", {
     responsive: true,
     global_stats: true,
     tooltips: true,
-  });
+});
+
+// project-image hover events
+let projectImg = document.querySelectorAll(".project-img");
+for (let i = 0; i < projectImg.length; i++) {
+    projectImg[i].addEventListener("mouseover", function () {
+        projectImg[i].firstElementChild.style.display = "none";
+        projectImg[i].lastElementChild.style.display = "block";
+        console.log("static_img.style.display")
+    });
+
+    projectImg[i].addEventListener("mouseout", function () {
+        projectImg[i].firstElementChild.style.display = "block";
+        projectImg[i].lastElementChild.style.display = "none";
+        console.log("out")
+    });
+}
+
+
+// fade-in effect while scrolling
+document.addEventListener("scroll", function () {
+    let pageTop = document.documentElement.scrollTop;
+    let pageBottom = pageTop + window.innerHeight;
+    let tags = document.querySelectorAll(".tag");
+
+    for (let i = 0; i < tags.length; i++) {
+        let tag = tags[i];
+        if (tag.getBoundingClientRect().top < pageBottom) {
+            tag.classList.add("visible");
+        } else {
+            tag.classList.remove("visible");
+        }
+    }
+});
+
