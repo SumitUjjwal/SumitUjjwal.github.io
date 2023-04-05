f2d8c53035de737bba4632a6c9cf34402f6b96e1
SumitUjjwal <sumitonboard@gmail.com>
2023-04-05 09:48:06 +0530

    delete: removed unwanted items from assets

--- script/index.js
+++ script/index.js
@@ -107,30 +107,30 @@ let projectImg = document.querySelectorAll(".project-img");
 //     });
 // }
 
-let projectcard = document.querySelectorAll(".project-card");
-for (let i = 0; i < projectcard.length; i++) {
-    projectcard[i].addEventListener("mouseover", () => {
-        projectImg[i].firstElementChild.style.display = "none";
-        projectImg[i].lastElementChild.style.display = "block";
-    })
-    projectcard[i].addEventListener("mouseout", () => {
-        projectImg[i].firstElementChild.style.display = "block";
-        projectImg[i].lastElementChild.style.display = "none";
-    })
-    // for (let i = 0; i < projectImg.length; i++) {
-    //     projectImg[i].addEventListener("mouseover", function () {
-    //         projectImg[i].firstElementChild.style.display = "none";
-    //         projectImg[i].lastElementChild.style.display = "block";
-    //         console.log("static_img.style.display")
-    //     });
+// let projectcard = document.querySelectorAll(".project-card");
+// for (let i = 0; i < projectcard.length; i++) {
+//     projectcard[i].addEventListener("mouseover", () => {
+//         projectImg[i].firstElementChild.style.display = "none";
+//         projectImg[i].lastElementChild.style.display = "block";
+//     })
+//     projectcard[i].addEventListener("mouseout", () => {
+//         projectImg[i].firstElementChild.style.display = "block";
+//         projectImg[i].lastElementChild.style.display = "none";
+//     })
+//     // for (let i = 0; i < projectImg.length; i++) {
+//     //     projectImg[i].addEventListener("mouseover", function () {
+//     //         projectImg[i].firstElementChild.style.display = "none";
+//     //         projectImg[i].lastElementChild.style.display = "block";
+//     //         console.log("static_img.style.display")
+//     //     });
 
-    //     projectImg[i].addEventListener("mouseout", function () {
-    //         projectImg[i].firstElementChild.style.display = "block";
-    //         projectImg[i].lastElementChild.style.display = "none";
-    //         console.log("out")
-    //     });
-    // }
-}
+//     //     projectImg[i].addEventListener("mouseout", function () {
+//     //         projectImg[i].firstElementChild.style.display = "block";
+//     //         projectImg[i].lastElementChild.style.display = "none";
+//     //         console.log("out")
+//     //     });
+//     // }
+// }
 
 // fade-in effect while scrolling
 document.addEventListener("scroll", function () {
